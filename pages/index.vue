5d5c6e06372d8554be5d9364164783cc51c2f641
ThiagoFragaK <thiagofraga@proton.me>
2025-02-25 19:41:01 -0300

    Add: Index page as homepage

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ProductsListComponent />
+    </div>
+</template>
+
+<script>
+    import ProductsListComponent from "@/components/products/ProductsListComponent.vue";
+
+    export default {
+        components: {
+            ProductsListComponent,
+        },
+   }
+</script>
