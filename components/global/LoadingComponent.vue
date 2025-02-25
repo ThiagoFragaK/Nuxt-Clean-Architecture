4f6c26bb85a58f847d32138aeaeb4f8bb9f45e03
ThiagoFragaK <thiagofraga@proton.me>
2025-02-25 20:16:52 -0300

    Refactor: Resize loading component

--- components/global/LoadingComponent.vue
+++ components/global/LoadingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="d-flex justify-content-center align-items-center">
         <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
