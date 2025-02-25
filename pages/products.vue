73c857611ec5133d9119eb5c64bc9987276457ed
ThiagoFragaK <thiagofraga@proton.me>
2025-02-25 19:41:31 -0300

    Add: Product page for specific product

---
+++ pages/products.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ProductComponent />
+    </div>
+</template>
+
+<script>
+    import ProductComponent from "@/components/products/ProductComponent.vue";
+    export default {
+        components: {
+            ProductComponent,
+        },
+        props: {
+            product: {
+                type: Object,
+                required: true,
+            }
+        },
+        mounted() {
+            console.log("Producsts")
+        }
+    }
+</script>
