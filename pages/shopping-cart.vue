47c71cea26d697af15d3fb016afcfee6cee1fcf7
ThiagoFragaK <thiagofraga@proton.me>
2025-03-17 20:26:58 -0300

    Add: ShoppingCart page

---
+++ pages/shopping-cart.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <ShoppingCartComponent />
+    </div>
+</template>
+
+<script>
+    import ShoppingCartComponent from '~/components/products/ShoppingCartComponent.vue';
+    export default {
+        components: {
+            ShoppingCartComponent,
+        },
+    }
+</script>
